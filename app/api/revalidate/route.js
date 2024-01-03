1edbb8320d0a658d65be43528717b5c65c977015
petruchevsky <m.berdichevsky.a@gmail.com>
2024-01-03 12:07:34 -0300

    revalidate 5

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -5,9 +5,17 @@ export function POST() {
 
   console.log("Running Webhook...")
   // revalidateTag('mi-etiqueta-de-cache');
-  revalidatePath('/', 'layout');
-  // revalidatePath('/testimonials', 'page');
-  // revalidatePath('/consultations', 'page');
+  revalidatePath('/', 'page');
+  revalidatePath('/consultations', 'page');
+  revalidatePath('/fees', 'page');
+  revalidatePath('/blog', 'page');
+  revalidatePath('/contact', 'page');
+  revalidatePath('/faq', 'page');
+  revalidatePath('/gallery', 'page');
+  revalidatePath('/homeopathy', 'page');
+  revalidatePath('/privacy-police', 'page');
+  revalidatePath('/terms-and-conditions', 'page');
+  revalidatePath('/testimonials', 'page');
 
   return NextResponse.json({ message: 'Cache revalidated' });
 }
